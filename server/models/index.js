a12daf03a358c1eb0f77f225e6c08abeb72a6f8a
Joe Fisher <josephfish1973@gmail.com>
2022-10-27 06:27:05 -0400

    added shipping eddress

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+const Orders = require('./shopping/Orders');
+const Rescues = require('./Rescues');
+const User = require('./shopping/User');
+const Product = require('./shopping/Product');
+const Category = require('./shopping/Category');
+
+module.exports = {
+    Orders,
+    Rescues,
+    User,
+    Product,
+    Category
+};
